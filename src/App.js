a7012da7a917fbe8b404b5f4b14465ad85eb22cd
rakibulfarhad <mraisfa@gmail.com>
2021-10-10 16:16:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './componants/Home/Home';
+import Login from './componants/Login/Login';
+import Register from './componants/Register/Register';
+import Header from './componants/Header/Header';
+import AuthProvider from './context/AuthProvider';
+import Shipping from './componants/Shipping/Shipping';
+import PrivateRoute from './componants/PrivateRoute/PrivateRoute';
+import PlaceOrder from './componants/PlaceOrder/PlaceOrder';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/register">
+              <Register></Register>
+            </Route>
+            <PrivateRoute path="/shipping">
+              <Shipping></Shipping>
+            </PrivateRoute>
+            <PrivateRoute path="/placeorder">
+              <PlaceOrder></PlaceOrder>
+            </PrivateRoute>
+          </Switch>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCl_GKO-jXr6fagjm86jhWjqhQ6ya4JDBs",
+    authDomain: "react-firebase-authentic-7855e.firebaseapp.com",
+    projectId: "react-firebase-authentic-7855e",
+    storageBucket: "react-firebase-authentic-7855e.appspot.com",
+    messagingSenderId: "963129656757",
+    appId: "1:963129656757:web:33bedd17a826aa532fc5fc"
+  };
+
+  export default firebaseConfig;
